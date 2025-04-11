3acb5733cc4aa7df636bc4983fbdc40372e5dc79
Sabirovar <robiya.s99@mail.ru>
2025-04-11 12:08:19 +0500

    Create python1.py

---
+++ lesson-1/homework/python1.py
@@ -0,0 +1,47 @@
+# Task 1
+
+side = input("Enter the side: ")
+side = float(side)
+
+perimiter = 4 * side
+area = side * side
+
+print(f"Perimiter: {perimiter}")
+print(f"Area: {area}")
+
+# Task 2
+string_diameter = input("Enter the diameter: ")
+float_diameter = float(string_diameter)
+
+
+radius = float_diameter / 2
+pi = 3.14
+
+length = 2 * pi * radius
+
+print(f"Circle length: {length}")
+
+# Task 3
+
+a = input("Enter a: ")
+a = float(a)
+
+b = input("Enter b: ")
+b = float(b)
+
+mean = (a + b) / 2
+
+print(f"Mean: {mean}")
+
+# Task 4
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+
+sum = a + b
+product = a * b
+square = (a + b) ** 2
+
+print(f"Sum: {sum}")
+print(f"Product: {product}")
+print(f"Square: {square}")
